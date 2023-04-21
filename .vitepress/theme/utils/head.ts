5df58dba8f114ebf31783f8e734bca398760af10
李鹏坤 <mail@lipk.org>
2023-04-21 14:38:39 +0800

    feat: head 增加 console-info

--- .vitepress/theme/utils/head.ts
+++ .vitepress/theme/utils/head.ts
@@ -1,5 +1,4 @@
 import { type HeadConfig } from "vitepress";
-
 import pkg from "vitepress/package.json";
 
 export const head: HeadConfig[] = [
@@ -96,5 +95,22 @@ export const head: HeadConfig[] = [
       })()
     `,
     // <script id="register-sw">;(() => { if ('serviceWorker'in navigator) { navigator.serviceWorker.register('/sw.js'); } })() </script>
+  ],
+  [
+    "script",
+    { id: "console-info" },
+    `;(() => {
+        console.info("\n%c 🎉 嗨~ 想看源代码？ %c \nGitHub仓库: https://github.com/zsdycs/lipk.org \n%c自主%c、%c跨界%c、%c终身学习%c。",
+        "color: #fff; padding: 5px 0; background: #29c75f; margin: 1em 0;",
+        "display: block;margin-left: 0.5em; margin: 1em 0;",
+        "color:#E24432; font-size: 16px; margin: 1em 0;",
+        "color:#666666; font-size: 16px; margin: 1em 0;",
+        "color:#0E70ED; font-size: 16px; margin: 1em 0;",
+        "color:#666666; font-size: 16px; margin: 1em 0;",
+        "color:#009E5E; font-size: 16px; margin: 1em 0;",
+        "color:#666666; font-size: 16px; margin: 1em 0;");
+      })()
+    `,
+    // <script id="console-info">;(() => { console.info(……); })() </script>
   ],
 ];
