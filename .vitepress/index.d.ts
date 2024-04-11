c37b66a72ad7845d8d07885ff20419e393ff2901
zsdycs <mail@lipk.org>
2024-04-11 23:15:14 +0800

    更新 vue

--- .vitepress/index.d.ts
+++ .vitepress/index.d.ts
@@ -4,18 +4,6 @@ declare module "*.vue" {
   export default component;
 }
 
-declare module "@siteData" {
-  import type { SiteData } from "vitepress";
-  const data: SiteData;
-  export default data;
-}
-
-declare module "@theme/index" {
-  import type { Theme } from "vitepress";
-  const theme: Theme;
-  export default theme;
-}
-
 declare module "markdown-it-footnote";
 
 declare module "markdown-it-image-figures";
